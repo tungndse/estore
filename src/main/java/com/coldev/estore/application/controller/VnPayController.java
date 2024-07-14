42f4883b8b5e676cd41c8db766905ad4fcacc3bd
tungnd <tungndse@gmail.com>
2024-07-14 17:18:37 +0700

    Some VNPAY bean corpses

--- src/main/java/com/coldev/estore/application/controller/VnPayController.java
+++ src/main/java/com/coldev/estore/application/controller/VnPayController.java
@@ -1,38 +1,22 @@
 package com.coldev.estore.application.controller;
 
 
+import com.coldev.estore.domain.service.VNPAYService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @RestController
 @Log4j2
-@RequestMapping("/vnpay")
+@RequestMapping("/api/v1/vnpay")
 public class VnPayController {
 
-    @PostMapping("/callback")
-    public ResponseEntity<String> handleVnpayCallback(@RequestParam Map<String, String> params) {
-        // Log the incoming parameters for debugging
-        log.info("Received VNPAY callback: " + params);
 
-        // Validate the transaction status and signature here
-        String transactionStatus = params.get("vnp_ResponseCode");
-
-        if ("00".equals(transactionStatus)) {
-            // Transaction successful
-            // Update your order status in the database
-
-            return ResponseEntity.ok("Transaction successful");
-        } else {
-            // Transaction failed or canceled
-            return ResponseEntity.ok("Transaction failed");
-        }
-    }
 
 
 }
